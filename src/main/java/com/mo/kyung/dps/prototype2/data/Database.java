ba8791ed12d32e24aa8acedf1e5ae119d74c03c5
kyungmo_yang <kyungmo.yang2986@gmail.com>
2019-05-16 16:33:29 +0200

    cleaned data

--- src/main/java/com/mo/kyung/dps/prototype2/data/Database.java
+++ src/main/java/com/mo/kyung/dps/prototype2/data/Database.java
@@ -9,7 +9,6 @@ import java.util.TreeSet;
 import com.mo.kyung.dps.prototype2.data.datatypes.AccountUser;
 import com.mo.kyung.dps.prototype2.data.datatypes.ExchangeMessage;
 import com.mo.kyung.dps.prototype2.data.datatypes.Topic;
-import com.mo.kyung.dps.prototype2.exceptions.MissingEntryInDatabaseException;
 
 public class Database {
 	private static Set<AccountUser> users = new TreeSet<AccountUser>();
@@ -17,53 +16,54 @@ public class Database {
 	private static Map<String, AccountUser> connectedUsers = new TreeMap<String, AccountUser>(); //String is for the token
 	private static Set<ExchangeMessage> uploadedMessages = new TreeSet<ExchangeMessage>();
 	static {
-//		addUser(new AccountUser("Patrick", "GRIMBERG", "patrick", "grimberg"));
-//		addUser(new AccountUser("Jérôme", "DAZIANO", "jerome", "daziano"));
-//		addUser(new AccountUser("Maud", "CADORET", "maud", "cadoret"));
-//		addUser(new AccountUser("Hamza", "OUCHEIKH", "hamza", "oucheikh"));
-//		addUser(new AccountUser("Abdoulaye", "SOW", "abdoulaye", "sow"));
-//		addUser(new AccountUser("Pascal", "REINA", "pascal", "reina"));
-//		addUser(new AccountUser("Mohamad", "YASSINE", "mohamad", "yassine"));
-//		addUser(new AccountUser("Frédéric", "GANTOIS", "frederic", "gantois"));
-//		addUser(new AccountUser("Alexis", "LE ROY", "alexis", "leroy"));
-//		addUser(new AccountUser("Kyung Mo", "YANG", "kyungmo", "yang"));
-//		addUser(new AccountUser("Amar", "LANKRI", "amar", "lankri"));
-//		addUser(new AccountUser("Syed Farath", "SAYEED", "syedfarath", "sayeed"));
-//		addUser(new AccountUser("Benoît", "GRASSET", "benoit", "grasset"));
-//		addUser(new AccountUser("Nathan", "MARGUET", "nathan", "marguet"));
-//		addUser(new AccountUser("Didier", "TERRIER", "didier", "terrier"));
-//		addUser(new AccountUser("Antoine", "MUNCK", "antoine", "munck"));
-//		
-//		addTopic(new Topic("Administrator"));
-//		addTopic(new Topic("General"));
-//		addTopic(new Topic("Karren"));
-//		addTopic(new Topic("Stagiaires"));
-//		
-//		getUser("abdoulaye").subscribeToTopic(getTopic("Administrator"));
-//		getUser("abdoulaye").subscribeToTopic(getTopic("Stagiaires"));
-//		getUser("abdoulaye").subscribeToTopic(getTopic("Karren"));
-//		getUser("abdoulaye").subscribeToTopic(getTopic("Administrator"));
-//		getUser("didier").subscribeToTopic(getTopic("Administrator"));
-//		getUser("antoine").subscribeToTopic(getTopic("Administrator"));
-//		getUser("patrick").subscribeToTopic(getTopic("Administrator"));
-//		getUser("maud").subscribeToTopic(getTopic("Stagiaires"));
-//		getUser("alexis").subscribeToTopic(getTopic("Stagiaires"));
-//		getUser("hamza").subscribeToTopic(getTopic("Stagiaires"));
-//		getUser("kyungmo").subscribeToTopic(getTopic("Stagiaires"));
-//		getUser("jerome").subscribeToTopic(getTopic("Karren"));
+		addUser(new AccountUser("admin", "admin", "admin", "admin"));
+		addUser(new AccountUser("Patrick", "GRIMBERG", "patrick", "grimberg"));
+		addUser(new AccountUser("Jérôme", "DAZIANO", "jerome", "daziano"));
+		addUser(new AccountUser("Maud", "CADORET", "maud", "cadoret"));
+		addUser(new AccountUser("Hamza", "OUCHEIKH", "hamza", "oucheikh"));
+		addUser(new AccountUser("Abdoulaye", "SOW", "abdoulaye", "sow"));
+		addUser(new AccountUser("Pascal", "REINA", "pascal", "reina"));
+		addUser(new AccountUser("Mohamad", "YASSINE", "mohamad", "yassine"));
+		addUser(new AccountUser("Frédéric", "GANTOIS", "frederic", "gantois"));
+		addUser(new AccountUser("Alexis", "LE ROY", "alexis", "leroy"));
+		addUser(new AccountUser("Kyung Mo", "YANG", "kyungmo", "yang"));
+		addUser(new AccountUser("Amar", "LANKRI", "amar", "lankri"));
+		addUser(new AccountUser("Syed Farath", "SAYEED", "syedfarath", "sayeed"));
+		addUser(new AccountUser("Benoît", "GRASSET", "benoit", "grasset"));
+		addUser(new AccountUser("Nathan", "MARGUET", "nathan", "marguet"));
+		addUser(new AccountUser("Didier", "TERRIER", "didier", "terrier"));
+		addUser(new AccountUser("Antoine", "MUNCK", "antoine", "munck"));
+		
+		addTopic(new Topic("Administrator"));
+		addTopic(new Topic("General"));
+		addTopic(new Topic("Karren"));
+		addTopic(new Topic("Stagiaires"));
+		
+		getUser("abdoulaye").subscribeToTopic(getTopic("Administrator"));
+		getUser("abdoulaye").subscribeToTopic(getTopic("Stagiaires"));
+		getUser("abdoulaye").subscribeToTopic(getTopic("Karren"));
+		getUser("abdoulaye").subscribeToTopic(getTopic("Administrator"));
+		getUser("didier").subscribeToTopic(getTopic("Administrator"));
+		getUser("antoine").subscribeToTopic(getTopic("Administrator"));
+		getUser("patrick").subscribeToTopic(getTopic("Administrator"));
+		getUser("maud").subscribeToTopic(getTopic("Stagiaires"));
+		getUser("alexis").subscribeToTopic(getTopic("Stagiaires"));
+		getUser("hamza").subscribeToTopic(getTopic("Stagiaires"));
+		getUser("kyungmo").subscribeToTopic(getTopic("Stagiaires"));
+		getUser("jerome").subscribeToTopic(getTopic("Karren"));
 	}
 	public static Set<AccountUser> getUsers() {
 		return Collections.unmodifiableSet(users);
 	}
-	public static AccountUser getUser(String login) throws MissingEntryInDatabaseException {
+	public static AccountUser getUser(String login) {
 		for (AccountUser user : users) {
 			if (user.getLogin().equals(login)) {
 				return user;
 			}
 		}
-		throw new MissingEntryInDatabaseException(new StringBuilder("Login ").append(login).append(" does not exist").toString());
+		return null;//user does not exist
 	}
-	public static boolean addUser(AccountUser user) throws MissingEntryInDatabaseException {
+	public static boolean addUser(AccountUser user) {
 		if (users.add(user)) {
 			user.subscribeToTopic(getTopic("Administration"));
 			return true;
@@ -76,13 +76,13 @@ public class Database {
 	public static Set<Topic> getTopics() {
 		return Collections.unmodifiableSet(topics);
 	}
-	public static Topic getTopic(String topicName) throws MissingEntryInDatabaseException {
+	public static Topic getTopic(String topicName) {
 		for (Topic topic : topics) {
 			if (topic.getName().equals(topicName)) {
 				return topic;
 			}
 		}
-		throw new MissingEntryInDatabaseException(new StringBuilder("Topic ").append(topicName).append(" does not exist").toString());
+		return null; //topic does not exist
 	}
 	public static boolean addTopic(Topic topic) {
 		return topics.add(topic);
@@ -93,35 +93,31 @@ public class Database {
 	public static Map<String, AccountUser> getConnectedUsers() {
 		return Collections.unmodifiableMap(connectedUsers);
 	}
-	public static Topic getTopic(String topicName) throws MissingEntryInDatabaseException {
-		for (Topic topic : topics) {
-			if (topic.getName().equals(topicName)) {
-				return topic;
+	public static AccountUser getConnectedUser(String login) {
+		for (AccountUser user : connectedUsers.values()) {
+			if (user.getLogin().equals(login)) {
+				return user;
 			}
 		}
-		throw new MissingEntryInDatabaseException(new StringBuilder("Topic ").append(topicName).append(" does not exist").toString());
+		return null;//user does not exist
 	}
-	public static boolean addTopic(Topic topic) {
-		return topics.add(topic);
+	public static boolean addConnectedUser(AccountUser connectedUser, String token) {
+		if (!connectedUsers.containsValue(connectedUser)) {
+			connectedUsers.put(token, connectedUser);
+			return true;
+		}
+		return false;
 	}
-	public static boolean removeTopic(Topic topic) {
-		return topics.remove(topic);
+	public static boolean removeConnectedUser(AccountUser user, String token) {
+		return connectedUsers.remove(token, user);
 	}
 	public static Set<ExchangeMessage> getUploadedMessages() {
 		return Collections.unmodifiableSet(uploadedMessages);
 	}
-	public static Topic getTopic(String topicName) throws MissingEntryInDatabaseException {
-		for (Topic topic : topics) {
-			if (topic.getName().equals(topicName)) {
-				return topic;
-			}
-		}
-		throw new MissingEntryInDatabaseException(new StringBuilder("Topic ").append(topicName).append(" does not exist").toString());
+	public static boolean uploadMessaage(ExchangeMessage message) {
+		return uploadedMessages.add(message);
 	}
-	public static boolean addTopic(Topic topic) {
-		return topics.add(topic);
-	}
-	public static boolean removeTopic(Topic topic) {
-		return topics.remove(topic);
+	public static boolean deletedMessage(ExchangeMessage message) {
+		return uploadedMessages.remove(message);
 	}
 }
--- src/main/java/com/mo/kyung/dps/prototype2/data/datatypes/AccountUser.java
+++ src/main/java/com/mo/kyung/dps/prototype2/data/datatypes/AccountUser.java
@@ -4,42 +4,42 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.mo.kyung.dps.prototype2.data.Database;
-import com.mo.kyung.dps.prototype2.data.resources.UserDetailResource;
-
 public class AccountUser implements Comparable<AccountUser> {
 	private String firstName, lastName, login, password;
 	private Set<Topic> topics;
 	private String token;
+	public AccountUser() {
+		super();
+	}
+	public AccountUser(String firstName, String lastName, String login, String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.login = login;
+		this.password = password;
+		this.topics = new HashSet<Topic>();
+		this.token = "";
+	}
 	public String getFirstName() {
 		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
 	}
 	public String getLastName() {
 		return lastName;
 	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 	public String getLogin() {
 		return login;
 	}
-	public void setLogin(String login) {
+	private void setLogin(String login) {
 		this.login = login;
 	}
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
+	private void setPassword(String password) {
 		this.password = password;
 	}
 	public Set<Topic> getTopics() {
 		return Collections.unmodifiableSet(topics);
-	}
-	public void setTopics(Set<Topic> topics) {
-		this.topics = topics;
 	}
 	private boolean addTopic(Topic topic) {
 		return this.topics.add(topic);
@@ -53,40 +53,36 @@ public class AccountUser implements Comparable<AccountUser> {
 	public void setToken(String token) {
 		this.token = token;
 	}
+	@Override
+	public int compareTo(AccountUser anotherUser) {
+		return getLogin().compareTo(anotherUser.getLogin());
+	}
+	public void changeLogin(String newLogin) {
+		setLogin(newLogin);
+	}
+	public void changePassword(String newPassword) {
+		setPassword(newPassword);
+	}
+	public void logIn() {
+		if (!isConnected()) {
+			//encode "login@101@password" and set it as token
+		}
+	}
+	public void logOut() {
+		if (isConnected()) {
+			token="";
+		}
+	}
 	public boolean isConnected() {
 		return !token.isEmpty();
 	}
-	public AccountUser() {
-		super();
-	}
-	public AccountUser(String firstName, String lastName, String login, String password) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.login = login;
-		this.password = password;
-		this.topics = new HashSet<Topic>();
-		this.topics.add(Database.getTopic("Administration"));
-		this.token = "";
-	}
 	public boolean isInterestedIn(Topic topic) {
 		return topics.contains(topic);
-	}
-	public UserDetailResource buildUserDetailResource() {
-		Set<String> topicsNames = new HashSet<String>();
-		for (Topic topic : topics) {
-			topicsNames.add(topic.getName());
-		}
-		return new UserDetailResource(firstName, lastName, login, topicsNames);
 	}
 	public boolean subscribeToTopic(Topic topic) {
 		return (addTopic(topic) && topic.addSubscriber(this));
 	}
 	public boolean unsubscribe(Topic topic) {
 		return removeTopic(topic);
-	}
-	@Override
-	public int compareTo(AccountUser anotherUser) {
-		return getLogin().compareTo(anotherUser.getLogin());
 	}
 }
